fcf317c0f2fcbbc76684d051dd5b0a8c5bb8b873
RJBarker <ryan.barker@intralox.com>
2023-10-17 21:17:34 +0100

    app.py alterations made. station and tobs pages added.

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -43,6 +43,7 @@ def home():
     print("Request to homepage made...")
     return (
         f"Available Routes:<br/>"
+        "<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
         f"/api/v1.0/tobs<br/>"
@@ -69,6 +70,44 @@ def precip():
         precip_annual.append(precip_dict)
     
     return jsonify(precip_annual)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Query a list of stations - Return as JSON"""
+    print("Request to Station list data made...")
+    session
+
+    stations = session.query(Station).all()
+
+    session.close()
+
+    # Convert results to a dictionary
+    station_list = []
+    for station in stations:
+        main_dict = {}
+        main_dict['Station'] = station.station
+        main_dict['Station_Details'] = {
+            "ID":station.id,
+            "Name":station.name,
+            "Latitude":station.latitude,
+            "Longitude":station.longitude,
+            "Elevation":station.elevation
+        }
+        station_list.append(main_dict)
+
+    return jsonify(station_list)
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Query a most active station - Return tobs as JSON"""
+    print("Request for tobs of most active station made...")
+    session
+
+    tobs = session.query(Measurement.date, Measurement.tobs).filter(Measurement.station == "USC00519281").filter(Measurement.date >= dt.date(2016,8,18)).all()
+
+    session.close()
+
+    
 
 
 if __name__ == '__main__':
