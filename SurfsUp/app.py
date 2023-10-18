286c8737cabb54210288311722f2261119caf865
RJBarker <ryan.barker@intralox.com>
2023-10-18 13:36:29 +0100

    comments added to app.py code

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -37,6 +37,7 @@ app = Flask(__name__)
 #################################################
 # Flask Routes
 #################################################
+# Home Page
 @app.route("/")
 def home():
     """Homepage - List all available api routes."""
@@ -54,6 +55,8 @@ def home():
         f"/api/v1.0/yyyy-mm-dd/yyyy-mm-dd<br/>"
     )
 
+#----------------------------------------------------#
+# Precipitation Page
 @app.route("/api/v1.0/precipitation")
 def precip():
     """Query 12 Months of Precipitation data - Return as JSON"""
@@ -72,8 +75,11 @@ def precip():
         precip_dict['precipitation'] = prcp
         precip_annual.append(precip_dict)
     
+    # JSONify the precip_annual list
     return jsonify(precip_annual)
 
+#----------------------------------------------------#
+# Stations Page
 @app.route("/api/v1.0/stations")
 def stations():
     """Query a list of stations - Return as JSON"""
@@ -97,9 +103,12 @@ def stations():
             "Elevation":station.elevation
         }
         station_list.append(main_dict)
-
+    
+    # JSONify the Station_list list
     return jsonify(station_list)
 
+#----------------------------------------------------#
+# Tobs Page
 @app.route("/api/v1.0/tobs")
 def tobs():
     """Query a most active station - Return tobs as JSON"""
@@ -118,9 +127,11 @@ def tobs():
         tobs_dict['Temperature'] = tobs
         tobs_list.append(tobs_dict)
 
+    # JSONify the tobs_list list
     return jsonify(tobs_list)    
 
-
+#----------------------------------------------------#
+# Dynamic Page - Start Date only
 @app.route("/api/v1.0/<start>")
 def dynamic_1(start):
     """Dynamic query to retrieve data from the given start date - Min, Max, Avg Temp as JSON"""
@@ -128,6 +139,7 @@ def dynamic_1(start):
 
     # Convert start to date format
     try:
+        # If the date given by the user contains "-" between Year, Month, Day - Remove the character
         if (start.__contains__("-")):
             start = start.replace("-","")
         
@@ -135,6 +147,7 @@ def dynamic_1(start):
 
         session
 
+        # Variables for reference to temperature calculations
         tmin = func.min(Measurement.tobs)
         tmax = func.max(Measurement.tobs)
         tavg = func.avg(Measurement.tobs)
@@ -154,6 +167,7 @@ def dynamic_1(start):
             }
             temp_data.append(temp_dict)
 
+        # JSONify the temp_data list
         return jsonify(temp_data)
     
     # Exception handle if date given is in the incorrect format
@@ -161,7 +175,8 @@ def dynamic_1(start):
         return jsonify({"error": f"The specified date '{start}' is not in the correct format.",
                         "note": f"Place a date in the format: yyyy-mm-dd or yyyymmdd"}), 404
 
-
+#----------------------------------------------------#
+# Dynamic Page - Start and End date
 @app.route("/api/v1.0/<start>/<end>")
 def dynamic_2(start,end):
     """Dynamic query to retrieve data from the given start date to given end date - Min, Max, Avg Temp as JSON"""
@@ -169,6 +184,7 @@ def dynamic_2(start,end):
 
     # Convert start to date format
     try:
+        # If the date given by the user contains "-" between Year, Month, Day - Remove the character
         if (start.__contains__("-")) or (end.__contains__("-")):
             start = start.replace("-","")
             end = end.replace("-","")
@@ -176,10 +192,12 @@ def dynamic_2(start,end):
         s_date = dt.datetime.strptime(start, '%Y%m%d')
         e_date = dt.datetime.strptime(end, '%Y%m%d')
 
+        # Check if the end date given is greater than the start date
         if (e_date > s_date):
 
             session
 
+            # Variables for reference to temperature calculations
             tmin = func.min(Measurement.tobs)
             tmax = func.max(Measurement.tobs)
             tavg = func.avg(Measurement.tobs)
@@ -200,8 +218,10 @@ def dynamic_2(start,end):
                 }
                 temp_data.append(temp_dict)
 
+            # JSONify the temp_data list
             return jsonify(temp_data)
         
+        # If the end date is less than start date, return an error and prompt for dates to be changed
         else:
             return jsonify({"error":f"The end date '{end}' can not be less than the start date '{start}'. Please adjust your date values."}), 404
     
